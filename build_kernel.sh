677b4287cdb1061e053c256f851f4740f89f4c1c
ThreeFiveDisplays-Nano-PC-T3PLUS <88690798+ThreeFiveDisplays-Nano-PC-T3PLUS@users.noreply.github.com>
2022-02-01 16:16:33 -0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,5 +6,4 @@ make ARCH=arm64
 cd ..
 mv kernel/arch/arm64/boot/Image  device/friendlyelec/nanopi3/boot
 mv kernel/arch/arm64/boot/dts/nexell/s5p6818-nanopi3-rev*.dtb device/friendlyelec/nanopi3/boot
-mv kernel/logo.bmp
-device/friendlyelec/nanopi3/boot
+mv kernel/logo.bmp device/friendlyelec/nanopi3/boot
