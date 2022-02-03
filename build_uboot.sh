039ceea6686f8179b5c01ec0c3cf7d3fc89e158f
ThreeFiveDisplays-Nano-PC-T3PLUS <88690798+ThreeFiveDisplays-Nano-PC-T3PLUS@users.noreply.github.com>
2022-02-02 18:25:30 -0700

    Update build_uboot.sh

--- build_uboot.sh
+++ build_uboot.sh
@@ -2,4 +2,4 @@ cd u-boot
 make s5p6818_nanopi3_config
 make CROSS_COMPILE=aarch64-linux-
 cd ..
-mv u-boot/fip-nonsecure.img  device/friendlyelec/nanopi3/boot
+cp u-boot/fip-nonsecure.img  device/friendlyelec/nanopi3/boot
